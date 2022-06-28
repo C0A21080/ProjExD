d7c6ac6fad140e62843920be2d4daaf435a56c36
C0A21080 <c0a21080a5@edu.teu.ac.jp>
2022-06-28 17:13:39 +0900

    4限追加機能実装完了

--- Ex03/ex01/maze.py
+++ Ex03/ex01/maze.py
@@ -24,9 +24,10 @@ def main_proc():
             my, mx = my+delta[key][1], mx+delta[key][0]
     except:
         pass
-    cx, cy = mx*100+50, my*100+50
+    cx, cy = mx*50+25, my*50+25
     canvas.coords("tori", cx, cy)
     root.after(100, main_proc)
+    
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -34,14 +35,16 @@ if __name__ == "__main__":
     
     canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
     canvas.pack()
-    maze_bg = mm.make_maze(15, 9) #1:壁/0:床を表す二次元リスト
+    maze_bg = mm.make_maze(30, 18) #1:壁/0:床を表す二次元リスト
     mm.show_maze(canvas, maze_bg) #canvaにmaze_bgを描く
-    #print(maze_bg)
 
     tori = tk.PhotoImage(file="fig/4 (1).png")
     mx, my = 1, 1
-    cx, cy = mx*100+50, my*100+50
+    cx, cy = mx*50+25, my*50+25
     canvas.create_image(cx, cy, image=tori, tag="tori")
+    if cx == 250:
+        tori = tk.PhotoImage(file="fig/don.png")
+
 
     key = ""
     root.bind("<KeyPress>", key_down)
