b2b066d9564de6b311e81caee74133928a38beed
C0A21080 <c0a21080a5@edu.teu.ac.jp>
2022-07-05 16:55:00 +0900

    4限基本機能実装完了

--- Ex04/dodge_bomb.py
+++ Ex04/dodge_bomb.py
@@ -20,13 +20,21 @@ def main():
     kkimg_rct.center = 900, 400
 
     #練習5  爆弾
-    bmimg_sfc = pg.Surface((20, 20)) #Surface
-    bmimg_sfc.set_colorkey((0, 0, 0))
-    pg.draw.circle(bmimg_sfc, (255, 0, 0), (10, 10), 10)
-    bmimg_rct = bmimg_sfc.get_rect() #Rect
-    bmimg_rct.centerx = randint(0, screen_rct.width)
-    bmimg_rct.centery = randint(0, screen_rct.height)
-    vx, vy = +1, +1 #練習6
+    bm1img_sfc = pg.Surface((20, 20)) #Surface
+    bm1img_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bm1img_sfc, (255, 0, 0), (10, 10), 10)
+    bm1img_rct = bm1img_sfc.get_rect() #Rect
+    bm1img_rct.centerx = randint(0, screen_rct.width)
+    bm1img_rct.centery = randint(0, screen_rct.height)
+    vx1, vy1 = +3, +3 #練習6
+
+    bm2img_sfc = pg.Surface((20, 20)) #Surface
+    bm2img_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bm2img_sfc, (0, 0, 255), (10, 10), 1000)
+    bm2img_rct = bm2img_sfc.get_rect() #Rect
+    bm2img_rct.centerx = randint(0, screen_rct.width)
+    bm2img_rct.centery = randint(0, screen_rct.height)
+    vx2, vy2 = +3, +3
 
 
     while True:
@@ -38,29 +46,49 @@ def main():
             
         #練習4
         key_states = pg.key.get_pressed() #辞書
-        if key_states[pg.K_UP]    == True: kkimg_rct.centery -= 1
-        if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += 1
-        if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
-        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
+        if key_states[pg.K_UP]    == True: kkimg_rct.centery -= 10
+        if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += 10
+        if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 10
+        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 10
+
+        if key_states[pg.K_UP]    == True: bm1img_rct.centery -= 10
+        if key_states[pg.K_DOWN]  == True: bm1img_rct.centery += 10
+        if key_states[pg.K_LEFT]  == True: bm1img_rct.centerx -= 10
+        if key_states[pg.K_RIGHT] == True: bm1img_rct.centerx += 10
+
+        if key_states[pg.K_UP]    == True: bm2img_rct.centery += 15
+        if key_states[pg.K_DOWN]  == True: bm2img_rct.centery -= 15
+        if key_states[pg.K_LEFT]  == True: bm2img_rct.centerx += 15
+        if key_states[pg.K_RIGHT] == True: bm2img_rct.centerx -= 15
         #練習7
         if check_bound(kkimg_rct, screen_rct) != (1, 1): #領域外だったら
-            if key_states[pg.K_UP]    == True: kkimg_rct.centery += 1
-            if key_states[pg.K_DOWN]  == True: kkimg_rct.centery -= 1
-            if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx += 1
-            if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -= 1
+            if key_states[pg.K_UP]    == True: kkimg_rct.centery += 100
+            if key_states[pg.K_DOWN]  == True: kkimg_rct.centery -= 100
+            if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx += 100
+            if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -= 100
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
         #練習6
-        bmimg_rct.move_ip(vx, vy)
+        bm1img_rct.move_ip(vx1, vy1)
+        bm2img_rct.move_ip(vx2, vy2)
         #練習5
-        screen_sfc.blit(bmimg_sfc, bmimg_rct)
+        screen_sfc.blit(bm1img_sfc, bm1img_rct)
+        screen_sfc.blit(bm2img_sfc, bm2img_rct)
         #練習7
-        yoko, tate = check_bound(bmimg_rct, screen_rct)
-        vx *= yoko
-        vy *= tate
+        yoko, tate = check_bound(bm1img_rct, screen_rct)
+        vx1 *= yoko
+        vy1 *= tate
+
+        yoko, tate = check_bound(bm2img_rct, screen_rct)
+        vx2 *= yoko
+        vy2 *= tate
 
         #練習8
-        if kkimg_rct.colliderect(bmimg_rct): return
+        if kkimg_rct.colliderect(bm1img_rct): return
+
+        pg.display.update()
+        clock.tick(1000)
+        if kkimg_rct.colliderect(bm2img_rct): return
 
         pg.display.update()
         clock.tick(1000)
